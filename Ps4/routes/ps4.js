6596922a3ac84ab05b21ae5015f2fc1c171b0aac
ZhixingZhao <56173726+ZhixingZhao@users.noreply.github.com>
2020-11-19 12:28:18 -0500

    ps5

--- Ps4/routes/ps4.js
+++ Ps4/routes/ps4.js
@@ -4,9 +4,18 @@ const router = express.Router();
 const fetchoption = require('../Config/config');
 const Convert = require('../ConvertFunction');
 const redis = require('redis');
+const rejson = require('redis-rejson')
+const {promisify} = require('util');
+
+rejson(redis)
 const client = redis.createClient();
 
+const asyncGet = promisify(client.get).bind(client)
+const asyncSet = promisify(client.set).bind(client)
+const asyncExist = promisify(client.exists).bind(client)
+const asyncExpire = promisify(client.expire).bind(client)
 
+client.flushdb();
 
 const getData = async (CountryCode) => {
     let rawCovidData = await fetch(fetchoption.fetchoptions.url + CountryCode);
@@ -14,15 +23,31 @@ const getData = async (CountryCode) => {
     return cleanCovidData;
 }
 
-router.post('/', (req, res, next) => {
+router.post('/', async (req, res, next) => {
     let CountryName = req.body.CountryName
-    let CountryCode = Convert.getCountryCode(CountryName)
-    getData(CountryCode).then(data => {
-//        console.log(data.data)
-        res.render('CovidDataRender', data.data)
-
-    });
-
+    if (await asyncExist(CountryName)){
+        let data = await asyncGet(CountryName)
+        let response = {
+            Country: CountryName,
+            cashed: true,
+            data: JSON.parse(data)
+        }
+        res.send(response)
+    }
+    else{
+        let CountryCode = Convert.getCountryCode(CountryName)
+        getData(CountryCode).then(async covid19apicall => {
+            let covid19data = covid19apicall.data
+            let cache = await asyncSet(CountryName, JSON.stringify(covid19data))
+            cache = await asyncExpire(CountryName,15)
+            let response = {
+                Country:CountryName,
+                cashed: false,
+                data:covid19data
+            }
+            res.send(response)
+        })
+    }
 });
 
 
