f715c4f333e71d3a8e89d496c9609a0fd01c05f2
ZhixingZhao <56173726+ZhixingZhao@users.noreply.github.com>
2020-12-07 00:33:37 -0500

    PS6 Finished

---
+++ PS6/src/app/Data/datatype.ts
@@ -0,0 +1,15 @@
+export class CovidData {
+  // tslint:disable-next-line:variable-name
+  updated_at: string;
+  date: string;
+  deaths: number;
+  confirmed: number;
+  recovered: number;
+  // tslint:disable-next-line:variable-name
+  new_confirmed: number;
+  // tslint:disable-next-line:variable-name
+  new_recovered: number;
+  // tslint:disable-next-line:variable-name
+  new_deaths: number;
+  active: number;
+}
--- PS6/src/app/app.component.ts
+++ PS6/src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { USCoviddata } from './Data/UnitedStates';
+import { CovidData } from './Data/datatype';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'PS6';
+export class AppComponent implements OnInit{
+  title = 'Covid-19 Data';
+  CountryName: string;
+  UsData: CovidData[] = USCoviddata;
+  SelectedDate: CovidData;
+  isEmpty = true;
+
+  ngOnInit(): void {
+  }
+
+  setSelectedDate(Date: CovidData): void {
+    this.SelectedDate = Date;
+  }
+
+  CheckIfIsUs(): boolean {
+    if (this.CountryName === 'United States'){
+      return true;
+    }else{
+      return false;
+    }
+  }
+  IsEmpty(): boolean {
+    return this.isEmpty;
+  }
+
+  getCountryName(countrynameinput: any): void {
+    this.isEmpty = false;
+    this.CountryName = countrynameinput.value;
+  }
 }
